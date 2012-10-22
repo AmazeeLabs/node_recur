25ffccf7537d8318b8c6f914dc023b71dfd89065
mikestefanello <mike.stefanello@gmail.com>
2012-10-22 11:55:07 -0400

    Added hook to easily validate node recur options on the forms.

--- node_recur.api.php
+++ node_recur.api.php
@@ -30,3 +30,40 @@ function hook_node_recur_access_alter(&$access, $node) {
 function hook_node_recur_batch_redirect_alter(&$path, $nid = NULL) {
   $path = 'node/' . $nid . '/edit';
 }
+
+/**
+ * Implements hook_node_recur_validate_dates().
+ *
+ * This hook is invoked when validating the node recur date form. It will
+ * only be called if there are no recorded validation errors found by
+ * this module.
+ *
+ * @param $node
+ *   The node being recurred.
+ * @param $form_state
+ *   The form's form state array.
+ * @return
+ *   An array of errors to be printed to the screen. Each error is an 
+ *   array with the keys 'field' (the name of the field) and 'error'
+ *   (the message to print to the screen).
+ */
+function hook_node_recur_validate_dates($node, $form_state) {
+  $errors = array();
+  if ($node->type == 'class') {
+    if ($form_state['values']['option'] == 'days') {
+      if (isset($form_state['values']['days']['monday'])) {
+        $errors[] = array(
+          'field' => 'days',
+          'message' => t('Classes cannot occur on Monday.'),
+        );
+      }
+      if (isset($form_state['values']['days']['tuesday'])) {
+        $errors[] = array(
+          'field' => 'days',
+          'message' => t('Classes cannot occur on Tuesday.'),
+        );
+      }
+    }
+  }
+  return $errors;
+}
